912aeb57c84eb4d59915904790dd32696f487611
Gregory MONNIER <gmonnier@e3r6p18.42.fr>
2017-12-20 11:34:32 +0100

    norme

--- srcs/wolf.c
+++ srcs/wolf.c
@@ -1,7 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wolf.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmonnier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/20 11:34:15 by gmonnier          #+#    #+#             */
+/*   Updated: 2017/12/20 11:34:16 by gmonnier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <wolf.h>
 
+static void		init_env(t_env *env, char *filename)
+{
+	env->width = WIDTH;
+	env->height = HEIGHT;
+	env->mlx_ptr = mlx_init();
+	env->win_ptr = mlx_new_window(env->mlx_ptr, env->width,
+		env->height, "Wolf3D");
+	env->img = mlx_new_image(env->mlx_ptr, env->width, env->height);
+	env->tmp = mlx_get_data_addr(env->img, &env->bpp, &env->sl, &env->edn);
+	load_text(env, env->textures);
+	map_init(&(env->map), filename);
+	init_sprites(&(env->sprites), env->nb_sprite);
+	cam_init(&(env->cam), 66, 0);
+	env->inputs.can_fire = 1;
+	timer_init(&(env->timer));
+	env->is_alive = 1;
+}
+
 /*
-** malloc de la struct, creation du la window et de l'image puis appel aux init cam et map
+** malloc de la struct, creation du la window et de l'image puis appel
+** aux init cam et map
 ** si on fail le malloc ou que !action on free lenv
 */
 
@@ -9,39 +40,23 @@ t_env			*ft_use_env(int action, char *filename)
 {
 	static t_env		*env;
 
-	if (action == 1 || action == 2 || action == 3) // a changer?
+	if (action == 1)
 	{
 		env = (t_env*)ft_memalloc(sizeof(t_env));
 		if (env != 0)
-		{
-			env->width = WIDTH;
-			env->height = HEIGHT;
-			env->mlx_ptr = mlx_init();
-			env->win_ptr = mlx_new_window(env->mlx_ptr, env->width,
-				env->height, "Wolf3D");
-			env->img = mlx_new_image(env->mlx_ptr, env->width, env->height);
-			env->tmp = mlx_get_data_addr(env->img, &env->bpp, &env->sl, &env->edn);
-			load_text(env, env->textures);
-			map_init(&(env->map), filename);
-			init_sprites(&(env->sprites), env->nb_sprite);
-			cam_init(&(env->cam), 66, 0);
-			env->inputs.can_fire = 1;
-			timer_init(&(env->timer));
-			env->is_alive = 1;
-		}
+			init_env(env, filename);
 		else
 			action = 0;
 	}
 	if (action == 0 && env != 0)
-	{
 		ft_memdel((void**)&(env));
-	}
 	return (env);
 }
 
 /*
-** ici on itere pour draw ligne par ligne, expose_hook btw gere le drag and drop de la fenetre
-** apres avoir tous draw, on balance l'image
+** Expose_hook btw le drag le drag and drop de la fenetre
+** la fonction draw est appele avec le bon timer pour 60fps
+** tous les 10ieme de secondes, on update le game pour les mouvements
 */
 
 int				expose_hook(void *param)
@@ -53,35 +68,22 @@ int				expose_hook(void *param)
 	if (env && env->is_alive == 1)
 	{
 		get_next_time(&(env->timer));
-		//draw que quand necessaire pour avoir 60fps;
 		if (env->timer.delta >= 1)
 			draw_all(env);
-		//updategame every 1/10 seconde
 		if (env->timer.timer >= CLOCKS_PER_SEC / 10)
-		{
 			update_game(env);
-			//print_map(env->map);
-		}
-		// possibilite d'afficher les fps sur l'ecran
-		/*if (env->timer.timer >= CLOCKS_PER_SEC)
-		{
-			printf("FPS : %d\n", env->timer.ticks);
-			env->timer.ticks = 0;
-			env->timer.timer = 0;
-			print_map(env->map);
-		}*/
 	}
 	return (0);
 }
-
-/*a revoir*/
 
 void			reset(t_env *env)
 {
 	env->is_alive = 1;
 	printf("reset\n");
-	env->map.initial_map = (int*)ft_memcpy(env->map.initial_map, env->map.reset_map, (env->map.w * env->map.h) * sizeof(int));
-	env->map.data = (int*)ft_memcpy(env->map.data, env->map.reset_map, (env->map.w * env->map.h) * sizeof(int));
+	env->map.initial_map = (int*)ft_memcpy(env->map.initial_map,
+	env->map.reset_map, (env->map.w * env->map.h) * sizeof(int));
+	env->map.data = (int*)ft_memcpy(env->map.data, env->map.reset_map,
+	(env->map.w * env->map.h) * sizeof(int));
 	free(env->sprites);
 	free(env->sprites_order);
 	free(env->sprites_distance);
@@ -113,13 +115,13 @@ int				main(int argc, char **argv)
 		env = ft_use_env(1, argv[1]);
 		if (env != 0 && env->mlx_ptr != 0)
 		{
-			mlx_hook(env->win_ptr, KeyPress, KeyPressMask, key_hook_press, 0);
-			mlx_hook(env->win_ptr, KeyRelease, KeyReleaseMask, key_hook_release, 0);
-			mlx_hook(env->win_ptr, 17, 0, exit_hook, 0);
-			mlx_expose_hook(env->win_ptr, expose_hook, 0);
-			mlx_loop_hook(env->mlx_ptr, expose_hook, 0);
+			mlx_hook(WIN, KeyPress, KeyPressMask, key_hook_press, 0);
+			mlx_hook(WIN, KeyRelease, KeyReleaseMask, key_hook_release, 0);
+			mlx_hook(WIN, 17, 0, exit_hook, 0);
+			mlx_expose_hook(WIN, expose_hook, 0);
+			mlx_loop_hook(MLX, expose_hook, 0);
 			system("afplay sounds/bg_music.mp3 &");
-			mlx_loop(env->mlx_ptr);
+			mlx_loop(MLX);
 		}
 	}
 	ft_putstr("Usage : wolf3d <map>\n");
